d6dbb659b6c8e68d0bf36fcada0fe67f14ee1adf
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-03-06 13:47:27 +0100

    feat: add LoginRequestDto

---
+++ FilRougeRefait/DTO/LoginRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace CoVoyageur.API.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        //[RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+        [Required]
+        //[PasswordValidator]
+        public string? PassWord { get; set; }
+    }
+}
